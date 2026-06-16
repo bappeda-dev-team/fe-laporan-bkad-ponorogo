8d8de6fcd11b8a26e14b18cf4018888cb03951db
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-06-16 15:29:04 +0700

    api tpp

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080"
 const API_TIMKERJA = process.env.NEXT_PUBLIC_API_TIMKERJA!
+const API_TPP = process.env.NEXT_PUBLIC_API_TPP!
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -9,6 +10,10 @@ const nextConfig: NextConfig = {
       {
         source: "/api/v1/timkerja/:path*",
         destination: `${API_TIMKERJA}/:path*`
+      },
+      {
+        source: "/api/v1/tpp/:path*",
+        destination: `${API_TPP}/:path*`
       },
       {
         source: "/api/v1/:path*",
